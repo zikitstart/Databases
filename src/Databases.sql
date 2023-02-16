8443d0a19dc558b3b476c21c91d8e5f2b3890c4e
zikit <zikit.start@yandex.ru>
2023-02-16 13:45:44 +0300

    Сделал дз2.

--- src/Databases.sql
+++ src/Databases.sql
@@ -112,3 +112,134 @@ skypro-# ORDER BY Максимальный_возраст;
  Анна     |                   42
  Григорий |                   58
 (2 строки)
+
+
+
+skypro=# CREATE TABLE city(city_id BIGSERIAL NOT NULL PRIMARY KEY,
+skypro(# city_name VARCHAR(60);
+CREATE TABLE
+skypro=# SELECT * FROM city;
+city_id | city_name
+---------+-----------
+(0 строк)
+
+
+skypro=# ALTER TABLE employee
+skypro-# ADD city_id INT NULL;
+ALTER TABLE
+skypro=# SELECT * FROM employee;
+id | first_name | last_name | gender | age | city_id
+----+------------+-----------+--------+-----+---------
+  5 | Григорий   | Сергеев   | М      |  20 |
+  6 | Анна       | Серова    | Ж      |  37 |
+  8 | Илья       | Степанов  | М      |  23 |
+  9 | Анна       | Котова    | Ж      |  42 |
+  7 | Григорий   | Петров    | М      |  58 |
+(5 строк)
+
+
+skypro=# ALTER TABLE employee
+skypro-# ADD CONSTRAINT fk_city_id FOREIGN KEY (city_id) REFERENCES city(city_id);
+ALTER TABLE
+
+
+skypro=# INSERT INTO city (city_name) VALUES ('Москва');
+INSERT 0 1
+skypro=# INSERT INTO city (city_name) VALUES ('Новгород');
+INSERT 0 1
+skypro=# INSERT INTO city (city_name) VALUES ('Рязань');
+INSERT 0 1
+skypro=# SELECT * FROM city;
+city_id | city_name
+---------+-----------
+       1 | Москва
+       2 | Новгород
+       3 | Рязань
+(3 строки)
+
+
+skypro=# UPDATE employee SET city_id = 1 WHERE id = 5;
+UPDATE 1
+skypro=# UPDATE employee SET city_id = 1 WHERE id = 6;
+UPDATE 1
+skypro=# UPDATE employee SET city_id = 2 WHERE id = 7;
+UPDATE 1
+skypro=# UPDATE employee SET city_id = 1 WHERE id = 8;
+UPDATE 1
+skypro=# SELECT * FROM employee;
+id | first_name | last_name | gender | age | city_id
+----+------------+-----------+--------+-----+---------
+  9 | Анна       | Котова    | Ж      |  42 |
+  5 | Григорий   | Сергеев   | М      |  20 |       1
+  6 | Анна       | Серова    | Ж      |  37 |       1
+  7 | Григорий   | Петров    | М      |  58 |       2
+  8 | Илья       | Степанов  | М      |  23 |       1
+(5 строк)
+
+
+skypro=# SELECT first_name,last_name,city_name
+skypro-# FROM employee
+skypro-# INNER JOIN city
+skypro-# ON employee.city_id = city.city_id;
+first_name | last_name | city_name
+------------+-----------+-----------
+ Григорий   | Сергеев   | Москва
+ Анна       | Серова    | Москва
+ Григорий   | Петров    | Новгород
+ Илья       | Степанов  | Москва
+(4 строки)
+
+
+skypro=# SELECT city_name,first_name,last_name
+skypro-# FROM employee
+skypro-# LEFT JOIN city
+skypro-# ON employee.city_id = city.city_id
+skypro-# ORDER by city_name;
+city_name | first_name | last_name
+-----------+------------+-----------
+ Москва    | Григорий   | Сергеев
+ Москва    | Анна       | Серова
+ Москва    | Илья       | Степанов
+ Новгород  | Григорий   | Петров
+           | Анна       | Котова
+(5 строк)
+
+
+skypro=# SELECT first_name,city_name
+skypro-# FROM employee
+skypro-# FULL JOIN city
+skypro-# ON employee.city_id = city.city_id
+skypro-# ORDER by first_name;
+first_name | city_name
+------------+-----------
+ Анна       |
+ Анна       | Москва
+ Григорий   | Москва
+ Григорий   | Новгород
+ Илья       | Москва
+            | Рязань
+(6 строк)
+
+
+skypro=# SELECT first_name,city_name
+skypro-# FROM employee
+skypro-# CROSS JOIN city;
+first_name | city_name
+------------+-----------
+ Анна       | Москва
+ Григорий   | Москва
+ Анна       | Москва
+ Григорий   | Москва
+ Илья       | Москва
+ Анна       | Новгород
+ Григорий   | Новгород
+ Анна       | Новгород
+ Григорий   | Новгород
+ Илья       | Новгород
+ Анна       | Рязань
+ Григорий   | Рязань
+ Анна       | Рязань
+ Григорий   | Рязань
+ Илья       | Рязань
+(15 строк)
+
